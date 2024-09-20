84ca98d0cf32e520d24ad26fe0bf66c0ac5df42a
Tushar yadav <0710tushar.2003@gmail.com>
2024-09-20 16:12:46 +0530

    message

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import home1 from '../Assets/home1.png'
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <div className='grid grid-cols-3 items-center p-32'>
+                <div className='col-span-2 '>
+                    <p className='text-4xl '>
+                        <span className='text-6xl font-bold'>Building Bridges</span>
+                        <br/>
+                        <span className='text-7xl font-normal text-[#6C6C6C]'>Between You And</span>
+                        <br/>
+                        
+                        <span className='text-7xl font-normal text-[#6C6C6C]'>Customers</span>
+                        <br/>
+                        
+                    </p>
+                    <p className='mt-7 pr-60 text-[#747474]'>Business ideas at the right price. Challenge everything. Keeping advertising standards high. Research based advertising for the bulls.</p>
+                    <button className='bg-[#DF73FF] text-white text-lg font-semibold px-9  py-3 mt-5 rounded-lg'>Get Started</button>
+                    
+                    
+                </div>
+                <div className='col-span-1'>
+                    <img src={home1} className='w-full h-auto' />
+                </div>
+            </div>
+
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { Route, RouterProvider } from 'react-router-dom';
-import { createBrowserRouter, createRoutesFromChildren } from 'react-router-dom';
-import BaseLayout from './BaseLayout';
-import Home from './Pages/Home';
-import Chat from './Pages/Chat';
-import Login from './Pages/Login';
-import Signup from "./Pages/Signup"
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { UserProvider } from './Context/MyContext';
-import Choose_Profession from './Pages/Onboarding/Choose_Profession';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+} from "react-router-dom";
+import BaseLayout from "./BaseLayout";
+import Home from "./Pages/Home";
+import Chat from "./Pages/Chat";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { UserProvider } from "./Context/MyContext";
+import Choose_Profession from "./Pages/Onboarding/Choose_Profession";
+import Freeprofile from "./Pages/Freeprofile";
+import Mygig from "./Pages/Mygig";
 
 const router = createBrowserRouter(
   createRoutesFromChildren(
     <Route>
-      <Route path='/' element={<BaseLayout />} >
-        <Route path='' element={<Home />} />
-        <Route path='chat' element={<Chat />} />
-        <Route path='login' element={<Login />} />
-        <Route path='signup' element={<Signup />} >
-        <Route path='choose-profession' element={<Choose_Profession/>}/>
+      <Route path="/" element={<BaseLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="chat" element={<Chat />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />}>
+          <Route path="choose-profession" element={<Choose_Profession />} />
+          
         </Route>
+        <Route path="freelancerprofile" element={<Freeprofile />} />
+        <Route path="mygig" element={<Mygig />} />
+
       </Route>
     </Route>
   )
-)
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
@@ -36,7 +45,6 @@ root.render(
         <RouterProvider router={router} />
       </GoogleOAuthProvider>
     </UserProvider>
-
   </React.StrictMode>
 );
 
